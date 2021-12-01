0222052bb72516b6d28109333c614700b4c2ef4c
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-12-01 18:38:54 -0500

    lo que nos encargaron

---
+++ loQueNosEncargaron.v
@@ -0,0 +1,171 @@
+module decode (
+	clk,
+	reset,
+	Op,
+	Funct,
+	Rd,
+	FlagW,
+	PCS,
+	NextPC,
+	RegW,
+	MemW,
+	IRWrite,
+	AdrSrc,
+	ResultSrc,
+	ALUSrcA,
+	ALUSrcB,
+	ImmSrc,
+	RegSrc,
+	ALUControl
+);
+	input wire clk;
+	input wire reset;
+	input wire [1:0] Op;
+	input wire [5:0] Funct;
+	input wire [3:0] Rd;
+	output wire [1:0] FlagW;
+	output wire PCS;
+	output wire NextPC;
+	output wire RegW;
+	output wire MemW;
+	output wire IRWrite;
+	output wire AdrSrc;
+	output wire [1:0] ResultSrc;
+	output wire [1:0] ALUSrcA;
+	output wire [1:0] ALUSrcB;
+	output wire [1:0] ImmSrc;
+	output wire [1:0] RegSrc;
+	output wire [1:0] ALUControl;
+	wire Branch;
+	wire ALUOp;
+
+	// Main FSM
+	mainfsm fsm(
+		.clk(clk),
+		.reset(reset),
+		.Op(Op),
+		.Funct(Funct),
+		.IRWrite(IRWrite),
+		.AdrSrc(AdrSrc),
+		.ALUSrcA(ALUSrcA),
+		.ALUSrcB(ALUSrcB),
+		.ResultSrc(ResultSrc),
+		.NextPC(NextPC),
+		.RegW(RegW),
+		.MemW(MemW),
+		.Branch(Branch),
+		.ALUOp(ALUOp)
+	);
+
+	// ADD CODE BELOW
+	// Add code for the ALU Decoder and PC Logic.
+	// Remember, you may reuse code from previous labs.
+	// ALU Decoder
+
+	// PC Logic
+
+
+	// Add code for the Instruction Decoder (Instr Decoder) below.
+	// Recall that the input to Instr Decoder is Op, and the outputs are
+	// ImmSrc and RegSrc. We've completed the ImmSrc logic for you.
+
+	// Instr Decoder
+	assign ImmSrc = Op;
+endmodule
+
+module mainfsm (
+	clk,
+	reset,
+	Op,
+	Funct,
+	IRWrite,
+	AdrSrc,
+	ALUSrcA,
+	ALUSrcB,
+	ResultSrc,
+	NextPC,
+	RegW,
+	MemW,
+	Branch,
+	ALUOp
+);
+	input wire clk;
+	input wire reset;
+	input wire [1:0] Op;
+	input wire [5:0] Funct;
+	output wire IRWrite;
+	output wire AdrSrc;
+	output wire [1:0] ALUSrcA;
+	output wire [1:0] ALUSrcB;
+	output wire [1:0] ResultSrc;
+	output wire NextPC;
+	output wire RegW;
+	output wire MemW;
+	output wire Branch;
+	output wire ALUOp;
+	reg [3:0] state;
+	reg [3:0] nextstate;
+	reg [12:0] controls;
+	localparam [3:0] FETCH = 0;
+	localparam [3:0] BRANCH = 9;
+	localparam [3:0] DECODE = 1;
+	localparam [3:0] EXECUTEI = 7;
+	localparam [3:0] EXECUTER = 6;
+	localparam [3:0] MEMADR = 2;
+	localparam [3:0] UNKNOWN = 10;
+
+	// state register
+	always @(posedge clk or posedge reset)
+		if (reset)
+			state <= FETCH;
+		else
+			state <= nextstate;
+	
+
+	// ADD CODE BELOW
+  	// Finish entering the next state logic below.  We've completed the 
+  	// first two states, FETCH and DECODE, for you.
+
+  	// next state logic
+	always @(*)
+		casex (state)
+			FETCH: nextstate = DECODE;
+			DECODE:
+				case (Op)
+					2'b00:
+						if (Funct[5])
+							nextstate = EXECUTEI;
+						else
+							nextstate = EXECUTER;
+					2'b01: nextstate = MEMADR;
+					2'b10: nextstate = BRANCH;
+					default: nextstate = UNKNOWN;
+				endcase
+			EXECUTER:
+			EXECUTEI:
+			MEMADR:
+			MEMRD:
+			default: nextstate = FETCH;
+		endcase
+
+	// ADD CODE BELOW
+	// Finish entering the output logic below.  We've entered the
+	// output logic for the first two states, FETCH and DECODE, for you.
+
+	// state-dependent output logic
+	always @(*)
+		case (state)
+			FETCH: controls = 13'b1000101001100;
+			DECODE: controls = 13'b0000001001100;
+			EXECUTER: 
+			EXECUTEI: 
+			ALUWB: 
+			MEMADR: 
+			MEMWR: 
+			MEMRD: 
+			MEMWB: 
+			BRANCH: 
+			default: controls = 13'bxxxxxxxxxxxxx;
+		endcase
+	assign {NextPC, Branch, MemW, RegW, IRWrite, AdrSrc, ResultSrc, ALUSrcA, ALUSrcB, ALUOp} = controls;
+endmodule
